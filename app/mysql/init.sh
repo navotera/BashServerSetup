c28fee799b6678bcccfe70eac7e58d4dd6639818
navotera <navotera@gmail.com>
2022-08-27 11:50:54 +0800

    add init

---
+++ app/mysql/init.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/sh
+wget https://github.com/navotera/BashServerSetup/raw/master/app/mysql/downgrade_mysql.sh | bash
+chmod +x downgrade_mysql.sh
+bash downgrade_mysql.sh
+rm downgrade_mysql.sh
