50364c181c25568a2426d5e5e4f85234d7bd6e25
navotera <navotera@gmail.com>
2022-08-27 11:57:53 +0800

    s

--- app/mysql/init_downgrade.sh
+++ app/mysql/init_downgrade.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
-wget https://github.com/navotera/BashServerSetup/raw/master/app/mysql/downgrade_mysql.sh | bash
+wget https://github.com/navotera/BashServerSetup/raw/master/app/mysql/downgrade_mysql.sh 
 chmod +x downgrade_mysql.sh
 bash downgrade_mysql.sh
 rm downgrade_mysql.sh
