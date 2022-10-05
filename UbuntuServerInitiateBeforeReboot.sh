a1c172ac99cfec3cb274c99860e668ac45b044d6
Muhammad Hendra <navotera@gmail.com>
2022-10-05 11:55:08 +0800

    add nfs-common

--- UbuntuServerInitiateBeforeReboot.sh
+++ UbuntuServerInitiateBeforeReboot.sh
@@ -23,11 +23,12 @@ sudo -E apt-get -qy autoclean
 apt-get install imagemagick -y ; apt install libmagickwand-dev imagemagick php-dev -y ; printf "\n" | pecl install imagemagick
 apt install libdigest-perl-md5-perl
 #others utilites
-apt install htop -y ; apt install ncdu -y ; apt install zip -y ; apt install screen -y
+apt install htop -y ; apt install ncdu -y ; apt install zip -y ; apt install screen -y ; apt install nfs-common -y
 
 #mod_geoip2 to allow disable access from another country
 #https://dev.maxmind.com/geoip/legacy/mod_geoip2/
 apt install libapache2-mod-geoip -y
+
 
 #change timezone
 sudo timedatectl set-timezone Asia/Makassar
