6cb2bee9b1c1dafe330c30887e560ee785efbcd7
Muhammad Hendra <hendra@akuntansipoliban.ac.id>
2022-10-19 08:26:25 +0800

    t

--- app/modsecurity/v2/init.sh
+++ app/modsecurity/v2/init.sh
@@ -1,3 +1,2 @@
-cd /tmp && wget https://github.com/navotera/BashServerSetup/blob/master/app/modsecurity/v2/install_modsecurity2.yml
-
+cd /tmp && wget https://github.com/navotera/BashServerSetup/raw/master/app/modsecurity/v2/install_modsecurity2.yml
 ansible-playbook install_modsecurity2.yml
