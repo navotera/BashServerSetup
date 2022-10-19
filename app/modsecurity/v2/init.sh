2fc67549a29395bab39b0dea3e4f47ec14ff04b5
Muhammad Hendra <hendra@akuntansipoliban.ac.id>
2022-10-19 11:28:55 +0800

    add binbash

--- app/modsecurity/v2/init.sh
+++ app/modsecurity/v2/init.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+#curl https://github.com/navotera/BashServerSetup/raw/master/app/modsecurity/v2/init.sh | bash
 cd /tmp && wget https://github.com/navotera/BashServerSetup/raw/master/app/modsecurity/v2/install_modsecurity2.yml
 apt install ansible && ansible-playbook /tmp/install_modsecurity2.yml
