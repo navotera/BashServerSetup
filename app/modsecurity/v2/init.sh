7a507c6bb6474754d41447e3e9d7d4a4e77ec8c4
Muhammad Hendra <hendra@akuntansipoliban.ac.id>
2022-10-21 16:44:31 +0800

    add update python

--- app/modsecurity/v2/init.sh
+++ app/modsecurity/v2/init.sh
@@ -5,5 +5,13 @@ wget https://github.com/navotera/BashServerSetup/raw/master/app/modsecurity/v2/i
 apt update
 apt install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
+add-apt-repository --yes --update ppa:deadsnakes/ppa
+apt-get update
+apt-get install python3.6 && apt-get install python3.7 && apt-get install python3.8
+apt install python3-pip
+
+cd /usr/bin && ls -lrth python* && unlink python && ln -s /usr/bin/python3.8 python
+cd /usr/bin && ls -lrth python* && unlink python3 && ln -s /usr/bin/python3.8 python3
+
 
 apt install ansible -y && ansible-playbook /tmp/install_modsecurity2.yml
