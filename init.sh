f1c69d42713a35fe02df4b6ba0386c859aecfa1c
Muhammad Hendra <hendra@akuntansipoliban.ac.id>
2024-01-24 09:00:59 +0800

    w

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+RED=`tput setaf 1`
+NC=`tput sgr0` # No Color
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+
 #in ubuntu 22.04 should change this to disable interactive mode that stop automatic process in this script
 sed -i 's/#$nrconf{restart} = '\''i'\'';/\$nrconf{restart} = '\''a'\'';/' /etc/needrestart/needrestart.conf
 
@@ -10,16 +15,19 @@ apt install software-properties-common -y
 add-apt-repository --yes --update ppa:ansible/ansible
 add-apt-repository --yes --update ppa:deadsnakes/ppa
 apt-get update
+
+echo "${GREEN}installing python 3.9 and 3.12..${NC}"
 apt-get apt-get install python3.9 -y && apt-get install python3.12 -y
 apt install python3-pip -y
 
-cd /usr/bin && ls -lrth python* && unlink python && ln -s /usr/bin/python3.12 python
-cd /usr/bin && ls -lrth python* && unlink python3 && ln -s /usr/bin/python3.12 python3
+#cd /usr/bin && ls -lrth python* && unlink python && ln -s /usr/bin/python3.12 python
+#cd /usr/bin && ls -lrth python* && unlink python3 && ln -s /usr/bin/python3.12 python3
 
 #harus direstart klo kd error 
 service apport restart
 systemctl restart systemd-timedated
 
+echo "${GREEN}installing ansible..${NC}"
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/init.yml
 
 
