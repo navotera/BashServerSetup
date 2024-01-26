2bbc7d866042bdd8fc3f84880d0274eb8eefb522
navotera <navotera@gmail.com>
2024-01-26 10:26:20 +0800

    c

--- playbook/sh/install_virtualmin.sh
+++ playbook/sh/install_virtualmin.sh
@@ -1,1 +1,2 @@
-sudo wget http://software.virtualmin.com/gpl/scripts/install.sh && sudo VIRTUALMIN_NONINTERACTIVE=1 /bin/sh install.sh --minimal --force --hostname "$HOSTNAME"
+HostName=grep '^HOSTNAME=' ~/server.config | cut -d'=' -f2
+sudo wget http://software.virtualmin.com/gpl/scripts/install.sh && sudo VIRTUALMIN_NONINTERACTIVE=1 /bin/sh install.sh --minimal --force --hostname "$HostName"
