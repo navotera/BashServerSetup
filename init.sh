1aa2c76e93dcdea03b575d40686eeb0504dd4cc0
navotera <navotera@gmail.com>
2024-01-26 11:16:14 +0800

    s

--- init.sh
+++ init.sh
@@ -29,6 +29,7 @@ systemctl restart systemd-timedated
 
 echo "${GREEN}installing ansible..${NC}"
 #apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/init.yml
+sh /tmp/BashServerSetup/playbook/sh/create_server_config.sh
 sh /tmp/BashServerSetup/playbook/sh/install_virtualmin.sh
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/core.yml
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/finalize.yml
