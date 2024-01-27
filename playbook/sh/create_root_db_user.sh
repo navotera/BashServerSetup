349dffe52465dc4fdc2109fcc5e1946bbd8ba9d7
navotera <navotera@gmail.com>
2024-01-27 16:25:14 +0800

    s

--- playbook/sh/create_root_db_user.sh
+++ playbook/sh/create_root_db_user.sh
@@ -1,2 +1,17 @@
-PASSWD='abc'
-sudo echo "CREATE USER 'root_db'@'localhost' IDENTIFIED BY '$PASSWD'; ALTER USER 'root_db'@'localhost' IDENTIFIED WITH mysql_native_password BY '$PASSWD'; GRANT ALL PRIVILEGES ON * . * TO 'root_db'@'localhost'; FLUSH PRIVILEGES;" | mysql  
+#!/bin/bash
+
+# Set new root password
+NEW_PASSWORD=$(grep '^PASSWORD=' /root/server.config | cut -d'=' -f2)
+echo $NEW_PASSWORD
+
+# Connect to MariaDB without a password 
+mysql -u root <<EOF
+CREATE USER 'root_db'@'localhost' IDENTIFIED BY '$NEW_PASSWORD';
+ALTER USER 'root_db'@'localhost' IDENTIFIED BY '$NEW_PASSWORD'; 
+GRANT ALL PRIVILEGES ON *.* TO 'root_db'@'localhost'; 
+FLUSH PRIVILEGES;
+EOF
+
+echo "MariaDB root_db password changed to $NEW_PASSWORD"
+echo "root_db user is created!"
+
