bee3dff6784f17736c6e639c2340a7310a7d0dfe
navotera <navotera@gmail.com>
2024-01-28 12:54:03 +0800

    d

--- playbook/sh/install_virtualmin.sh
+++ playbook/sh/install_virtualmin.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-HostName=grep '^HOSTNAME=' ~/server.config | cut -d'=' -f2
+HostName=$(grep '^HOSTNAME=' ~/server.config | cut -d'=' -f2)
 sudo wget -O /tmp/install_virtualmin.sh http://software.virtualmin.com/gpl/scripts/install.sh
+RETRY_COUNT=3
 
+install_virtualmin() {
+    VIRTUALMIN_NONINTERACTIVE=1 /bin/sh /tmp/install_virtualmin.sh --minimal --force --hostname "$HostName"
+}
 
-# install virtualmin, it can be failed for GPG, so it should try again. 
-command_to_execute="sudo VIRTUALMIN_NONINTERACTIVE=1 /bin/sh /tmp/install_virtualmin.sh --minimal --force --hostname "$HostName""
+# Retry loop
+for i in $(seq 1 $RETRY_COUNT); do
+    echo "Attempt $i to install Virtualmin"
+    install_virtualmin
 
-# Set the maximum number of retries
-max_retries=3
-retries=0
+    # Check the exit status
+    if [ $? -eq 0 ]; then
+        echo "Installation successful."
+        break  # Exit the loop if successful
+    else
+        echo "Installation failed. Retrying..."
+    fi
 
-# Execute the command with retry
-while [ $retries -lt $max_retries ]
-do
-  # Execute the command
-  $command_to_execute
-
-  # Check the exit status of the command
-  if [ $? -eq 0 ]; then
-    # If the command was successful, break out of the loop
-    echo "Command executed successfully"
-    break
-  else
-    # If the command failed, increment the retry count
-    retries=$((retries+1))
-    echo "Command failed. Retrying... (Attempt $retries of $max_retries)"
-  fi
+    # Sleep for a while before the next attempt (adjust as needed)
+    sleep 5
 done
