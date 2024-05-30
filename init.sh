392a0eff0bcd0cd83a496e251a98eef25b3fd006
navotera <navotera@gmail.com>
2024-05-30 08:05:35 +0800

    c

--- init.sh
+++ init.sh
@@ -5,6 +5,22 @@ NC=`tput sgr0` # No Color
 GREEN=`tput setaf 2`
 YELLOW=`tput setaf 3`
 
+
+
+echo "Select an option:"
+echo "1. Install Apache2 with Virtualmin (default)"
+echo "2. Install Nginx with Virtualmin"
+read -p "Enter your choice (1 or 2, or press Enter for default) within 10 seconds: " -t 10 choice
+
+# Set the default choice if no input is provided within 10 seconds
+if [ -z "$choice" ]; then
+    echo "No choice made within 10 seconds. Proceeding with the default option (1)."
+    choice=1
+fi
+
+
+
+
 #in ubuntu 22.04 should change this to disable interactive mode that stop automatic process in this script
 sed -i 's/#$nrconf{restart} = '\''i'\'';/\$nrconf{restart} = '\''a'\'';/' /etc/needrestart/needrestart.conf
 
@@ -39,7 +55,31 @@ systemctl restart systemd-timedated
 
 echo "${GREEN}installing ansible..${NC}"
 #apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/init.yml
-sh /tmp/BashServerSetup/playbook/sh/create_server_config.sh && sh /tmp/BashServerSetup/playbook/sh/install_virtualmin.sh
+
+
+sh /tmp/BashServerSetup/playbook/sh/create_server_config.sh 
+
+case $choice in
+    1)
+        sh /tmp/BashServerSetup/playbook/sh/install_virtualmin_apache2.sh
+        ;;
+    2)
+        sh /tmp/BashServerSetup/playbook/sh/install_virtualmin_nginx.sh
+        ;;
+    *)
+        echo "Invalid choice. Exiting..."
+        exit 1
+        ;;
+esac
+
+#setup the swap for 1G
+sudo swapoff /swapfile
+sudo fallocate -l 2G /swapfile
+ls -lh /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/core.yml
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/optimization.yml
 apt install ansible -y && ansible-playbook /tmp/BashServerSetup/playbook/finalize.yml
